f027bfa01f485246cf8c1d34f9f4ec20650d1989
DenisOzindzheDev <denis.ozindzhe@tages.ru>
2024-01-09 09:20:28 +0300

    other json test

---
+++ leetcode/max_score/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+func maxScore(s string) int {
+
+	return 0
+}
---
+++ other/json_test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Config struct {
+	Test string `json:"test,omitempty"`
+	Mama string `json:"mama,omitempty"`
+}
+
+func main() {
+	c := Config{
+		Test: "test",
+		Mama: "mama",
+	}
+	test, _ := json.Marshal(c) //serialize
+	json.NewDecoder()
+	fmt.Printf("aa %v", json.Unmarshal(test, c))
+}
